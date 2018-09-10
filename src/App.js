34e9b894ce339eae4f30c895ab0f0f7354c93b5d
emadhuri579@gmail.com <emadhuri579@gmail.com>
2018-09-10 15:33:34 -0700

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+import Customers from './components/customers/coustomers'; 
+import Landing from './components/landing/landing';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to Postmates</h1>
+        </header>
+        <Router>
+        <div>
+          <Route exact path='/' component={Landing}/>
+          <Route path='/Landing' component = {Landing} />
+          <Route path='/Customers' component ={Customers} />
+        </div>  
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
